c1eb58be544c3305436ed9ad764bb4789e64d699
silvertiti <metheotim@gmail.com>
2024-11-15 16:45:55 +0100

    Txt -> tableau

--- jeux de la vie/jeux de la vie.cpp
+++ jeux de la vie/jeux de la vie.cpp
@@ -15,49 +15,43 @@ void affichageTableau(int* grill, int lignes, int colonnes) {
 }
 
 int* creationTableau(int a, int b) {
-    int lignes = a;
-    int colonnes = b;
+    int* grill = new int[a * b]();
 
-    int* grill = new int[lignes * colonnes]();
-
-
-    for (int i = 0; i < lignes * colonnes; ++i) {
+    for (int i = 0; i < a * b; ++i) {
         grill[i] = 0;
     }
     return grill;
 }
 
-int main() {
-    string cheminFichier = "C:\\Users\\methe\\source\\repos\\jeux de la vie\\test.txt"; // Remplacez par le chemin de votre fichier
+bool lireFichier(const string& cheminFichier, string& ligne1, string& ligne2) {
     ifstream fichier(cheminFichier);
 
-    if (!fichier) {
-        cerr << "ferme le fichier fdp" << endl;
-        return 1;
+    if (getline(fichier, ligne1)) {
+        if (!getline(fichier, ligne2)) {
+            cerr << "y'a pas de deuxiéme ligne batard" << endl;
+            return false;
+        }
+    }
+    else {
+        cerr << "Ton fichier TXT est vide fdp" << endl;
+        return false;
     }
 
+    return true;
+}
+
+int main() {
+    string cheminFichier = "C:\\Users\\methe\\source\\repos\\jeux de la vie\\test.txt";
     string ligne1, ligne2;
-    if (getline(fichier, ligne1)) {
-    }
-    else {
-        cout << "casser sa marche pas " << endl;
-    }
-    if (getline(fichier, ligne2)) {
-        cout << "Premiere ligne : " << ligne2 << endl;
-    }
-    else {
-        cout << "casser sa marche pas 2" << endl;
-    }
+
+	lireFichier(cheminFichier, ligne1, ligne2);
+    
 
     int lignes = stoi(ligne1);
     int colonnes = stoi(ligne2);
+
     int* tableau = creationTableau(lignes, colonnes);
-
     affichageTableau(tableau, lignes, colonnes);
-
-
-
     delete[] tableau;
 
-    return 0;
 }
