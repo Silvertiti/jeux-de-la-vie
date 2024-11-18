6f8c213b789dc51787beebb1849a08d76205ef86
silvertiti <metheotim@gmail.com>
2024-11-18 09:56:39 +0100

    lecture du fichier txt 100% OP

--- jeux de la vie/jeux de la vie.cpp
+++ jeux de la vie/jeux de la vie.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-void affichageTableau(int* grill, int lignes, int colonnes) {
+void affichageTableau(int* grill, int lignes, int colonnes){  /// affichage du tableau
     cout << "Tableau de taille " << lignes << "x" << colonnes << " :\n";
     for (int i = 0; i < lignes; ++i) {
         for (int j = 0; j < colonnes; ++j) {
@@ -15,51 +16,95 @@ void affichageTableau(int* grill, int lignes, int colonnes) {
 }
 
 int* creationTableau(int a, int b) {
-    int* grill = new int[a * b]();
-
-    for (int i = 0; i < a * b; ++i) {
-        grill[i] = 0;
-    }
+	int* grill = new int[a * b](); // crée le tableau et initialise tout à 0
     return grill;
 }
 
-bool lireFichier(const string& cheminFichier, string& ligne1, string& ligne2) {
+bool lireDimensions(const string& cheminFichier, int& lignes, int& colonnes) {
     ifstream fichier(cheminFichier);
+    if (!fichier.is_open()) {
+        cerr << "FDP ferme le fichier" << endl;
+        return false;
+    }
 
-    if (getline(fichier, ligne1)) {
-        if (!getline(fichier, ligne2)) {
-            cerr << "y'a pas de deuxiéme ligne batard" << endl;
-            return false;
-        }
+    string ligne;
+
+	// associationd de la deuxiéme ligne pour les lignes
+    if (getline(fichier, ligne)) {
+        lignes = stoi(ligne);
     }
     else {
-        cerr << "Ton fichier TXT est vide fdp" << endl;
+        cerr << "Bon la premiére ligne est vide ou pas bonne" << endl;
+        return false;
+    }
+
+	// association de la deuxiéme ligne pour les colonnes
+    if (getline(fichier, ligne)) {
+        colonnes = stoi(ligne);
+    }
+    else {
+        cerr << "Bon la deuxiéme ligne est vide ou pas bonne" << endl;
         return false;
     }
 
     return true;
 }
 
-bool changerValeur(int* grill,int emplacement) {
-	grill[emplacement] = 1;
-    return grill;
+bool lireTableauDepuisFichier(const string& cheminFichier, int* tableau, int lignes, int colonnes) {
+    ifstream fichier(cheminFichier);
+    if (!fichier.is_open()) {
+        cerr << "ferme le fichier ou tu la suprimer" << endl;
+        return false;
+    }
+
+    string ligne;
+    int index = 0;
+
+	// sa saute les 2 premieres lignes car la on  cherche a lire le tableau 
+    for (int i = 0; i < 2; ++i) {
+        if (!getline(fichier, ligne)) {
+            cerr << "Gros con le fichier TXT est vide" << endl;
+            return false;
+        }
+    }
+
+    // début de la lecture du tableau
+    for (int i = 0; i < lignes; ++i) {
+        if (getline(fichier, ligne)) {
+            stringstream ss(ligne);
+            for (int j = 0; j < colonnes; ++j) {
+                int valeur;
+                if (ss >> valeur) {
+                    tableau[index++] = valeur;
+                }
+                else {
+                    cerr << "gros con ta pas mis le tableau dans le fichier txt " << endl;
+                    return false;
+                }
+            }
+        }
+        else {
+            cerr << "bon ton fichier est vide FDP" << endl;
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
     string cheminFichier = "C:\\Users\\methe\\source\\repos\\jeux de la vie\\test.txt";
-    string ligne1, ligne2;
+    int lignes = 0, colonnes = 0;
 
-	lireFichier(cheminFichier, ligne1, ligne2);
-    
-    int lignes = stoi(ligne1);
-    int colonnes = stoi(ligne2);
+    if (!lireDimensions(cheminFichier, lignes, colonnes)) {
+        return -1;
+    }
 
     int* tableau = creationTableau(lignes, colonnes);
-    affichageTableau(tableau, lignes, colonnes);
-	int emplacement = 40;
-	changerValeur(tableau, emplacement);
-	affichageTableau(tableau, lignes, colonnes);
 
+    lireTableauDepuisFichier(cheminFichier, tableau, lignes, colonnes);
+    affichageTableau(tableau, lignes, colonnes);
+    
 
     delete[] tableau;
+    return 0;
 }
